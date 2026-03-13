d7d9caa4c17e1a707252d4fa36727380fba35bd0
Mateusz-Birembaut <mateusz.birembaut@etu.umontpellier.fr>
2026-03-13 15:03:51 +0100

    using textManager get for shot detail and fixed padding on note

--- FormTextEditWidget.cpp
+++ FormTextEditWidget.cpp
@@ -11,11 +11,12 @@ FormTextEditWidget::FormTextEditWidget(const QString &name, const QString &text,
 
     m_nameLabel = new QLabel(this);
     m_nameLabel->setAlignment(Qt::AlignLeft);
-    m_nameLabel->setStyleSheet("font-weight: bold;");
+    m_nameLabel->setStyleSheet("font-weight: bold; padding-left: 10px;");
     m_nameLabel->setText(name);
 
     m_textEdit = new FormTextEdit(text, editable, this);
     m_textEdit->setAlignment(Qt::AlignRight);
+    m_textEdit->setStyleSheet("padding-left: 10px;");
     
     m_frameLayout->addWidget(m_nameLabel);
     m_frameLayout->addWidget(m_textEdit);

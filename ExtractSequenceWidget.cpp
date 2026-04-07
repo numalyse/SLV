386114682cd51be767bbd1d4b813d779ee6874e8
Mateusz-Birembaut <mateusz.birembaut@etu.umontpellier.fr>
2026-04-07 10:26:47 +0200

    Updates export path on export

--- ExtractSequenceWidget.cpp
+++ ExtractSequenceWidget.cpp
@@ -106,9 +106,12 @@ void ExtractSequenceWidget::onEndTimeChanged(const int newTime)
 
 void ExtractSequenceWidget::confirmExtraction()
 {
-    QString saveSequencePath = QFileDialog::getSaveFileName(this, tr("Extract sequence"));
+    auto& prefManager = PrefManager::instance();
+    QString saveSequencePath = QFileDialog::getSaveFileName(this, tr("Extract sequence"), prefManager.getPref("Paths", "lp_extract_sequence"));
     if(saveSequencePath != ""){
         QProcess* sequenceExtractor = SequenceExtractionHelper::extractSequence(m_media.filePath(), m_startTime, m_endTime, saveSequencePath + '.' + m_media.fileExtension());
         connect(sequenceExtractor, &QProcess::finished, this, &QDialog::accept);
+        QFileInfo fileInfo (saveSequencePath);
+        prefManager.setPref("Paths", "lp_extract_sequence", fileInfo.absolutePath());
     }
 }

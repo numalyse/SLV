4884fa44ced382ffb9aef80a1ff58e61666e8bb7
Christinamrn <58227221+Christinamrn@users.noreply.github.com>
2026-04-07 17:05:24 +0200

    Update DrawingWidget

--- DrawingWidget.cpp
+++ DrawingWidget.cpp
@@ -1,21 +1,62 @@
 #include "DrawingWidget.h"
 
+#include "PrefManager.h"
 #include <QPainter>
 #include <QPainterPath>
+#include <QHBoxLayout>
+#include <QFrame>
+#include <QMouseEvent>
 
 DrawingWidget::DrawingWidget(QWidget *parent)
     : QWidget(parent)
 {
-    setAttribute(Qt::WA_TransparentForMouseEvents);
+    //setAttribute(Qt::WA_TransparentForMouseEvents);
     setAttribute(Qt::WA_NoSystemBackground);
     setAttribute(Qt::WA_TranslucentBackground);
     setAutoFillBackground(false);
     setStyleSheet("background-color: rgba(0,0,0,0)");
     setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    setFocusPolicy(Qt::StrongFocus);
+
+    //initDrawingToolbar();
 }
+
+void DrawingWidget::initDrawingToolbar(){
+    m_drawingToolbar = new QWidget(this);
+    QFrame* containerBackground = new QFrame(m_drawingToolbar);
+    // m_drawingToolbar->setGeometry(50, m_mediaRect.height()/2, , 200);
+    containerBackground->setGeometry(50, m_mediaRect.height()-200-50, 50, 200);
+    m_drawingToolbar->setStyleSheet(
+        "QFrame {"
+        " background-color: palette(base);"
+        " border-radius: 6px;"
+        " border: 1px solid palette(button);"
+        "}"
+        "QLabel {"
+        " border: none;"
+        "}");
+    
+    m_penModeBtn = new ToolbarToggleButton(
+    this, 
+    false,
+    "auto_segmentation_white",
+    PrefManager::instance().getText("deactivate") + " " + PrefManager::instance().getText("tooltip_drawing_mode"),
+    "auto_segmentation",
+    PrefManager::instance().getText("activate") + " " + PrefManager::instance().getText("tooltip_drawing_mode")
+    );
+}
+
 void DrawingWidget::showDrawingMode(bool isEnabled)
 {
     m_isEnabled = isEnabled;
+    if (m_isEnabled){
+        initDrawingToolbar();
+        m_drawingToolbar->show();
+        m_drawingToolbar->raise();
+    } else {
+        m_drawingToolbar->hide();
+    }
+
     update(); // redraw
 }
 
@@ -37,21 +78,60 @@ void DrawingWidget::onMediaRectChanged(const QRect &rect)
     update();
 }
 
+void DrawingWidget::mousePressEvent(QMouseEvent *event)
+{
+    setFocus();
+    if (event->button() == Qt::LeftButton && m_isEnabled)
+    {
+        m_drawing = true;
+
+        QPoint p = event->pos() - m_mediaRect.topLeft();
+
+        QPainterPath newPath;
+        newPath.moveTo(p);
+
+        m_paths.append(newPath);
+    }
+}
+
+void DrawingWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if (m_drawing && !m_paths.isEmpty())
+    {
+        QPoint p = event->pos() - m_mediaRect.topLeft();
+
+        m_paths.last().lineTo(p);
+
+        update();
+    }
+}
+
+void DrawingWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        m_drawing = false;
+    }
+}
+
 void DrawingWidget::paintEvent(QPaintEvent *)
 {
     if (!m_isEnabled) return;
-    
+
     QPainter p(this);
     p.setRenderHint(QPainter::Antialiasing);
 
-    setColor(Qt::red);
-    QPen pen(m_color, m_lineWidth);
-    p.setPen(pen);
+    m_pen.setColor(Qt::red);
+    p.setPen(m_pen);
 
     p.translate(m_mediaRect.topLeft());
 
     int w = m_mediaRect.width();
     int h = m_mediaRect.height();
-    p.drawLine(w / 3, 0, w / 3, h);
+
+    for (const QPainterPath &path : m_paths)
+    {
+        p.drawPath(path);
+    }
 
 }
--- DrawingWidget.h
+++ DrawingWidget.h
@@ -1,8 +1,10 @@
 #ifndef DRAWINGWIDGET_H
 #define DRAWINGWIDGET_H
 
+#include "./ToolbarButtons/ToolbarToggleButton.h"
 #include <QWidget>
 #include <QSize>
+#include <QPen>
 
 class DrawingWidget : public QWidget
 {
@@ -15,17 +17,36 @@ public:
     void setColor(const QColor& color);
     void setLineWidth(int width);
 
+    void mousePressEvent(QMouseEvent *event);
+
+    void mouseMoveEvent(QMouseEvent *event);
+
+    void mouseReleaseEvent(QMouseEvent *event);
+
 public slots:
     void onMediaRectChanged(const QRect &rect);
 
 protected:
+    QWidget *m_drawingToolbar = nullptr;
+
     void paintEvent(QPaintEvent *event) override;
+    void initDrawingToolbar();
 
 private:
     QRect m_mediaRect;
     bool m_isEnabled = false;
+
+    QVector<QPainterPath> m_paths;
+    bool m_drawing = false;
+    QPoint m_lastPoint;
+
+    ToolbarToggleButton* m_penModeBtn = nullptr;
+
     QColor m_color = QColor(255, 255, 255, 120);
     int m_lineWidth = 2;
+    QPen m_pen {QColor(255, 255, 255, 120), 2};
+
+    
 };
 
 #endif // DRAWINGWIDGET_H

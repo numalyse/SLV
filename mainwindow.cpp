5a7a42155f1ef607301938546073544fa707e605
Mateusz-Birembaut <mateusz.birembaut@etu.umontpellier.fr>
2026-04-08 11:00:37 +0200

    Updated lang location to interface to simplify pref widget ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,7 +41,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     auto& prefManager = PrefManager::instance();
     prefManager.loadPrefs();
-    prefManager.loadLanguage(prefManager.getPref("Lang","code"));
+    prefManager.loadLanguage(prefManager.getPref("Interface","Lang","code"));
 
     auto *rootLayout = new QVBoxLayout(ui->centralwidget);
     rootLayout->setContentsMargins(0,0,0,0);

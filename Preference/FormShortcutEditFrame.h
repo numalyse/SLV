192d1162de1f2c29ca7dde9626d5a991b36e323c
Mateusz-Birembaut <mateusz.birembaut@etu.umontpellier.fr>
2026-04-08 10:26:58 +0200

    Added comments

--- Preference/FormShortcutEditFrame.h
+++ Preference/FormShortcutEditFrame.h
@@ -13,15 +13,19 @@ Q_OBJECT
 public:
     FormShortcutEditFrame(const QString &name, const QString &subCategory, const QString &key, const QString &value, QWidget *parent);
 
+    /// @brief Empties the value of the key sequence edit and sets previous value to ""
     void clearShortcutUI();
+
+    /// @brief Updates the value of the key sequence edit and set previous value to the shortcut given
     void setShortcutUI(const QString &shortcut);
 
 signals :
     void updateJsonObjRequested(const QString& subCategory, const QString& key, const QString& newShortcutString);
     void emptyShortcutUIRequested(const QString& key);
+
 private:
     QKeySequenceEdit* m_keySequenceEdit = nullptr;
-    QString m_prevValue;
+    QString m_prevValue; // when assigning keys and conflict occurs, give to ability to set the key back to its state before the assignment 
 
 
 };
--- Preference/ShortcutTab.h
+++ Preference/ShortcutTab.h
@@ -13,14 +13,23 @@ Q_OBJECT
 
 public:
     explicit ShortcutTab(QWidget* parent = nullptr);
+
+    /// @brief Checks whether one of the tab has been modified and needs to ask user to save
+    /// @return True if needs save
     bool needSave();
+
+    /// @brief Updates the json with all of the changes from all of the tabs
     void save();
+
+    /// @brief Reverts all changes of all tabs
     void discard();
 
     ~ShortcutTab() {}
 
 private:
-    QMap<QString, FormShortcutEditFrame*> m_shortcutFrames;
+    QMap<QString, FormShortcutEditFrame*> m_shortcutFrames; // map to easily access and update the value of each shortcuts (based on the key in the json ex : "enter_fullscreen") 
+
+    /// use of 2 json object to check if needing to save and be able to discard changes
     QJsonObject m_baseShortcut;
     QJsonObject m_updatedShortcut;
 

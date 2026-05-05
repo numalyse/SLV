afdc124d3be025c904c8f5ec93424ed2d1b92f92
Christinamrn <58227221+Christinamrn@users.noreply.github.com>
2026-05-05 12:26:30 +0200

    Update lang preference texts combobox

--- Preference/Frames/FormComboBoxFrame.cpp
+++ Preference/Frames/FormComboBoxFrame.cpp
@@ -1,19 +1,40 @@
 #include "FormComboBoxFrame.h"
+#include "PrefManager.h"
 
 FormComboBoxFrame::FormComboBoxFrame(const QString &name, const QString &subCategory, const QString &key, const QString &value, const QStringList& values, QWidget *parent)
 : BasePreferenceFrame(name, subCategory, key, value, parent)
 {
     m_comboBox = new QComboBox(this);
-    m_comboBox->addItems(values);
+    //m_comboBox->addItems(values);
 
-    m_comboBox->setCurrentText(value); 
+    for (const QString &code : values)
+    {
+        m_comboBox->addItem(
+            PrefManager::instance().getText(code), 
+            code                                    
+        );
+    }
 
+    int index = m_comboBox->findData(value);
+    if (index != -1)
+    {
+        m_comboBox->setCurrentIndex(index);
+    }
+
+    m_comboBox->model()->sort(0);
     setRightWidget(m_comboBox);
 
-    connect(m_comboBox, &QComboBox::currentTextChanged, this, [this](const QString& newText){
-        if (newText != m_prevValue) {
-            m_prevValue = newText;
-            emit updateJsonObjRequested(m_subCategory, m_key, newText);
+    connect(m_comboBox,
+            &QComboBox::currentIndexChanged,
+            this,
+            [this](int)
+    {
+        QString newValue = m_comboBox->currentData().toString();
+
+        if (newValue != m_prevValue)
+        {
+            m_prevValue = newValue;
+            emit updateJsonObjRequested(m_subCategory, m_key, newValue);
         }
     });
 

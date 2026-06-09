3a940fa0a13aa892bfd6aa78445bd0584c679ead
Mateusz-Birembaut <mateusz.birembaut@etu.umontpellier.fr>
2026-06-09 16:02:07 +0200

    Fix bug right click timeline no action selected

--- Timeline/TimelineWidget.cpp
+++ Timeline/TimelineWidget.cpp
@@ -391,12 +391,10 @@ void TimelineWidget::showContextMenuForShot(const QPoint& globalPos, ShotItem* i
     }
 
     QAction *selectedAction = menu.exec(globalPos);
-
-    if (selectedAction == actionSplit) {
+    if (!selectedAction) return;
         splitShotAtCursor();
     } else if (selectedAction == actionAB){
         ABAction();
-    } else if (selectedAction == deleteABMarkers) {
         m_abManager->deleteMarkers();
     } else if (selectedAction == actionExtractAB){
         m_abManager->extractLoop();

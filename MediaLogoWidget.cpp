1e85eddc6ed56f551a0736153d61e7d34c7a1789
Mateusz-Birembaut <60432147+Mateusz-Birembaut@users.noreply.github.com>
2026-06-17 14:51:18 +0200

    MediaLogoWidget no longer in a floating window above widget

--- MediaLogoWidget.cpp
+++ MediaLogoWidget.cpp
@@ -5,23 +5,19 @@
 MediaLogoWidget::MediaLogoWidget(QWidget *parent, const QString &iconPath, int maxWidth)
 : QWidget(parent)
 {
+    // pas besoin d'etre transparent donc on peut ne pas les mettre en fenetre flottante
     setAttribute(Qt::WA_TransparentForMouseEvents);
     setAttribute(Qt::WA_NoSystemBackground);
     setAttribute(Qt::WA_TranslucentBackground);
-    setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::Tool);
-    setWindowFlag(Qt::NoDropShadowWindowHint);
 
     QHBoxLayout* layout = new QHBoxLayout(this);
     m_logo = new AspectRatioPixmapLabel(this);
     m_logo->setMaximumWidth(maxWidth);
 
-
     QPixmap audioLogo(iconPath); 
     m_logo->setPixmap(audioLogo);
     m_logo->setAlignment(Qt::AlignCenter);
     layout->addWidget(m_logo);
-    
-
 }
 
 void MediaLogoWidget::onMediaRectChanged(const QRect &rect)

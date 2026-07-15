81dc640a54a1a1c32b217c27b680fb4504725899
Mateusz-Birembaut <60432147+Mateusz-Birembaut@users.noreply.github.com>
2026-07-15 15:45:08 +0200

    Update annotation widget
    mouse events :
    - click : setPosition at annot start time
    - double click : grow note label
    
    Update annotion via update button which grows the annotation and all fields are editable

--- AnnotationWidget.cpp
+++ AnnotationWidget.cpp
@@ -1,11 +1,16 @@
 #include "AnnotationWidget.h"
+#include "IconHelper.h"
 #include "PrefManager.h"
 #include "TimeFormatter.h"
 #include "Project/ProjectManager.h"
-#include <QMenu>
+#include "SignalManager.h"
+
 #include <QMouseEvent>
 #include <QGuiApplication>
 #include <QStyleHints>
+#include <QAbstractTextDocumentLayout>
+#include <QtMath>
+#include <QApplication>
 
 AnnotationWidget::AnnotationWidget(QWidget *parent, const Annotation &annotation)
     : QWidget{parent}, m_annotation(annotation)
@@ -30,19 +35,45 @@ AnnotationWidget::AnnotationWidget(QWidget *parent, const Annotation &annotation
     m_colorBar->setFixedWidth(6);
     mainLayout->addWidget(m_colorBar);
 
+    // column holding the top row and the note editor below it
+    QVBoxLayout *contentLayout = new QVBoxLayout();
+    contentLayout->setSpacing(4);
+
+    m_topRowLayout = new QHBoxLayout();
+    m_topRowLayout->setSpacing(8);
+
     // for start / end timecodes
-    QVBoxLayout *timeLayout = new QVBoxLayout();
-    timeLayout->setSpacing(2);
+    m_timeLayout = new QVBoxLayout();
+    m_timeLayout->setSpacing(2);
 
     m_startLabel = new QLabel("00:00:00.00");
     m_endLabel = new QLabel("00:00:00.00");
 
-    timeLayout->addWidget(m_startLabel);
-    timeLayout->addWidget(m_endLabel);
+    // fixed size so they stay in place when switching between display and edit mode
+    for(QLabel* timeLabel : {m_startLabel, m_endLabel}){
+        timeLabel->setAlignment(Qt::AlignCenter);
+        timeLabel->setFixedSize(90, 20);
+    }
 
-    mainLayout->addLayout(timeLayout);
+    m_startEdit = new QLineEdit();
+    m_endEdit = new QLineEdit();
+    for(QLineEdit* timeEdit : {m_startEdit, m_endEdit}){
+        timeEdit->setInputMask("99:99:99.99");
+        timeEdit->setAlignment(Qt::AlignCenter);
+        timeEdit->setFixedSize(90, 20);
+        timeEdit->hide();
+    }
 
-    // bloc titre / note
+    m_timeLayout->addWidget(m_startLabel);
+    m_timeLayout->addWidget(m_endLabel);
+    m_timeLayout->addWidget(m_startEdit);
+    m_timeLayout->addWidget(m_endEdit);
+
+    m_topRowLayout->addLayout(m_timeLayout);
+    // always pinned to the top so the timecodes don't move between modes
+    m_topRowLayout->setAlignment(m_timeLayout, Qt::AlignTop);
+
+    // bloc name / note
     QVBoxLayout *infoLayout = new QVBoxLayout();
     infoLayout->setSpacing(2);
 
@@ -51,37 +82,108 @@ AnnotationWidget::AnnotationWidget(QWidget *parent, const Annotation &annotation
 
     m_noteLabel = new QLabel();
     m_noteLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Preferred);
-    
+
+    m_nameEdit = new QLineEdit();
+    m_nameEdit->hide();
+
     infoLayout->addWidget(m_nameLabel);
     infoLayout->addWidget(m_noteLabel);
+    infoLayout->addWidget(m_nameEdit);
 
-    mainLayout->addLayout(infoLayout, 1);
-    mainLayout->addSpacing(10);
+    m_topRowLayout->addLayout(infoLayout, 1);
+    m_topRowLayout->addSpacing(10);
 
-    m_menuBtn = new ToolbarButton(this, "", "");
-    mainLayout->addWidget(m_menuBtn);
+    m_updateBtn = new ToolbarButton(this, "edit_white", PrefManager::instance().getText("edit"));
+
+    connect(m_updateBtn, &ToolbarButton::clicked, this, [this](){
+        if(m_mode != Mode::Edited)
+            setMode(Mode::Edited);
+    });
+
+    // fixed height so update btn stays in place when switching between display and edit mode
+    QWidget* updateBtnContainer = new QWidget(this);
+    updateBtnContainer->setFixedHeight(42);
+    QVBoxLayout* updateBtnLayout = new QVBoxLayout(updateBtnContainer);
+    updateBtnLayout->setContentsMargins(0,0,0,0);
+    updateBtnLayout->addStretch();
+    updateBtnLayout->addWidget(m_updateBtn);
+    updateBtnLayout->addStretch();
+
+    m_topRowLayout->addWidget(updateBtnContainer);
+    m_topRowLayout->setAlignment(updateBtnContainer, Qt::AlignTop);
+
+    contentLayout->addLayout(m_topRowLayout);
+
+    m_noteEdit = new QTextEdit();
+    m_noteEdit->setFixedHeight(80);
+    m_noteEdit->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    m_noteEdit->hide();
+    contentLayout->addWidget(m_noteEdit);
+
+    // grow the note editor with its content, queued so the resize happens later once everything is set
+    connect(m_noteEdit->document()->documentLayout(), &QAbstractTextDocumentLayout::documentSizeChanged,
+            this, &AnnotationWidget::adjustNoteEditHeight, Qt::QueuedConnection);
+
+    // color / confirm / cancel row 
+    QHBoxLayout *editActionsLayout = new QHBoxLayout();
+    editActionsLayout->setSpacing(8);
+
+    m_colorBtn = new QPushButton();
+    m_colorBtn->setFixedSize(60, 24);
+    m_colorBtn->setToolTip(PrefManager::instance().getText("tooltip_color_tool"));
+    m_colorBtn->hide();
+    connect(m_colorBtn, &QPushButton::clicked, this, &AnnotationWidget::openColorPicker);
+
+    m_confirmBtn = new ToolbarButton(this, "check_white", PrefManager::instance().getText("confirm"));
+    m_cancelBtn = new ToolbarButton(this, "cancel_white", PrefManager::instance().getText("cancel_action"));
+    m_deleteBtn = new ToolbarButton(this, "delete_white", PrefManager::instance().getText("delete"));
+
+    m_confirmBtn->hide();
+    m_cancelBtn->hide();
+    m_deleteBtn->hide();
+
+    connect(m_confirmBtn, &QPushButton::released, this, &AnnotationWidget::applyEdits);
+    connect(m_cancelBtn, &QPushButton::released, this, [this]{ setMode(Mode::Minimized); });
+    connect(m_deleteBtn, &QPushButton::released, this, [this]{ emit removeAnnotationRequested(m_annotation.id); });
+
+    editActionsLayout->addWidget(m_colorBtn);
+    editActionsLayout->addStretch();
+    editActionsLayout->addWidget(m_confirmBtn);
+    editActionsLayout->addWidget(m_cancelBtn);
+    editActionsLayout->addWidget(m_deleteBtn);
+
+    contentLayout->addLayout(editActionsLayout);
+
+    mainLayout->addLayout(contentLayout, 1);
+
+    m_clickTimer = new QTimer(this);
+    m_clickTimer->setSingleShot(true);
+    m_clickTimer->setInterval(QApplication::doubleClickInterval()); // double click detection speed based on os
+    connect(m_clickTimer, &QTimer::timeout, this, [this]{
+        emit SignalManager::instance().timelineSetPosition(m_annotation.start);
+    });
 
     initStyle();
     refreshContent();
-
-    connect(m_menuBtn, &ToolbarButton::clicked, this, &AnnotationWidget::openMenu);
 }
 
 void AnnotationWidget::initStyle()
 {
+    setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
     setFixedHeight(50);
     setAttribute(Qt::WA_StyledBackground, true);
     setContentsMargins(0,0,0,0);
     setStyleSheet("AnnotationWidget{border-style: solid; border: 1px solid " + m_palbtnColorStr + "; border-radius: 4px;}");
 
-    m_menuBtn->setFixedSize(24,24);
-    m_menuBtn->setMaximumWidth(20);
-    m_menuBtn->setStyleSheet("QPushButton{"
+    m_updateBtn->setFixedSize(24,24);
+    m_updateBtn->setMaximumWidth(20);
+
+    m_deleteBtn->setStyleSheet("QPushButton{"
         "   background-color: rgba(0,0,0,0);"
         "   border: none;"
         "}"
         "QPushButton:hover{"
-        "   background-color: " + m_palbtnColorStr + ";"
+        "   background-color: tomato;"
         "   border: 1px solid " + m_palbtnColorStr + ";"
         "   border-radius: 4px;"
         "}");
@@ -102,6 +204,92 @@ void AnnotationWidget::refreshContent()
     m_noteLabel->setToolTip(m_annotation.note);
 }
 
+void AnnotationWidget::setMode(Mode mode)
+{
+
+    m_mode = mode;
+    bool isEditMode = (m_mode == Mode::Edited);
+    
+    if (isEditMode) {
+        double fps = currentFps();
+        m_startEdit->setText(TimeFormatter::msToHHMMSSFF(m_annotation.start, fps));
+        m_endEdit->setText(TimeFormatter::msToHHMMSSFF(m_annotation.end, fps));
+        m_nameEdit->setText(m_annotation.name);
+        m_noteEdit->setPlainText(m_annotation.note);
+        m_editColor = m_annotation.color;
+        updateColorButton();
+    }
+
+    m_startLabel->setVisible(!isEditMode);
+    m_endLabel->setVisible(!isEditMode);
+    m_nameLabel->setVisible(!isEditMode);
+    m_noteLabel->setVisible(!isEditMode);
+
+    m_startEdit->setVisible(isEditMode);
+    m_endEdit->setVisible(isEditMode);
+    m_nameEdit->setVisible(isEditMode);
+    m_noteEdit->setVisible(isEditMode);
+    m_colorBtn->setVisible(isEditMode);
+    m_confirmBtn->setVisible(isEditMode);
+    m_cancelBtn->setVisible(isEditMode);
+    m_deleteBtn->setVisible(isEditMode);
+
+    if (isEditMode || m_mode == Mode::Extended) {
+        // release the fixed height
+        setMinimumHeight(50);
+        setMaximumHeight(QWIDGETSIZE_MAX);
+        adjustNoteEditHeight();
+    } else {
+        setFixedHeight(50);
+    }
+}
+
+void AnnotationWidget::applyEdits()
+{
+    Annotation updated = m_annotation;
+    double fps = currentFps();
+    updated.start = TimeFormatter::HHMMSSFFToMs(m_startEdit->text(), fps);
+    updated.end = TimeFormatter::HHMMSSFFToMs(m_endEdit->text(), fps);
+    updated.name = m_nameEdit->text();
+    updated.note = m_noteEdit->toPlainText();
+    updated.color = m_editColor;
+
+    setMode(Mode::Minimized);
+
+    emit updateAnnotationRequested(updated);
+}
+
+void AnnotationWidget::openColorPicker()
+{
+    IconHelper::execColorPickerMenu(this, m_colorBtn, [this](const QColor& color){
+        m_editColor = color;
+        updateColorButton();
+    });
+}
+
+void AnnotationWidget::updateColorButton()
+{
+    m_colorBtn->setStyleSheet("QPushButton{"
+        "   background-color: " + m_editColor.name() + ";"
+        "   border: 1px solid palette(mid);"
+        "   border-radius: 4px;"
+        "}");
+}
+
+void AnnotationWidget::adjustNoteEditHeight()
+{
+    if (m_mode == Mode::Minimized)
+        m_noteLabel->setWordWrap(false);
+    else if (m_mode == Mode::Edited){
+        int chrome = m_noteEdit->height() - m_noteEdit->viewport()->height();
+        int docHeight = qCeil(m_noteEdit->document()->size().height());
+        m_noteEdit->setFixedHeight(qMax(80, docHeight + chrome));
+    }else {
+        m_noteLabel->setWordWrap(true);
+    }
+
+}
+
 void AnnotationWidget::updateAnnotation(const Annotation &annotation)
 {
     m_annotation = annotation;
@@ -113,22 +301,8 @@ double AnnotationWidget::currentFps() const
     Media* media = ProjectManager::instance().media();
     if (media && media->fps() > 0.0)
         return media->fps();
-    return 25.0;
+    return 1.0;
 }
-
-void AnnotationWidget::openMenu()
-{
-    QMenu menu(this);
-    QAction *editAction = menu.addAction(PrefManager::instance().getText("edit"));
-    QAction *removeAction = menu.addAction(PrefManager::instance().getText("remove"));
-
-    QAction *chosenAction = menu.exec(m_menuBtn->mapToGlobal(QPoint(0, m_menuBtn->height())));
-    if (chosenAction == editAction)
-        emit editAnnotationRequested(m_annotation.id);
-    else if (chosenAction == removeAction)
-        emit removeAnnotationRequested(m_annotation.id);
-}
-
 
 void AnnotationWidget::enterEvent(QEnterEvent *event)
 {
@@ -138,5 +312,28 @@ void AnnotationWidget::enterEvent(QEnterEvent *event)
 void AnnotationWidget::leaveEvent(QEvent *event)
 {
     setStyleSheet("AnnotationWidget{border-style: solid; border: 1px solid " + m_palbtnColorStr + "; border-radius: 4px;}");
+}
+
+void AnnotationWidget::mouseReleaseEvent(QMouseEvent *event)
+{    
+    if(m_ignoreNextRelease){
+        m_ignoreNextRelease = false;
+        return;
+    }
+
+    m_clickTimer->start();
+}
+
+void AnnotationWidget::mouseDoubleClickEvent(QMouseEvent *event)
+{
+    // prevent mouse release event from happening
+    m_clickTimer->stop();
+    m_ignoreNextRelease = true;
+    
+    if (m_mode == Mode::Minimized){
+        setMode(Mode::Extended);
+    }else if (m_mode == Mode::Extended){
+        setMode(Mode::Minimized);
+    }
 }
 
--- AnnotationWidget.h
+++ AnnotationWidget.h
@@ -6,6 +6,8 @@
 #include <QPushButton>
 #include <QLayout>
 #include <QFrame>
+#include <QTextEdit>
+#include <QLineEdit>
 
 #include "Annotation.h"
 #include "ToolbarButtons/ToolbarButton.h"
@@ -18,7 +20,8 @@ public:
     explicit AnnotationWidget(QWidget *parent, const Annotation &annotation = {});
     void initStyle();
     int annotationId() const { return m_annotation.id; }
-
+    const Annotation& annot() { return m_annotation; }
+    
 public slots:
     void updateAnnotation(const Annotation &annotation);
 
@@ -26,13 +29,27 @@ signals:
     void annotationClicked(int annotationId);
     void editAnnotationRequested(int annotationId);
     void removeAnnotationRequested(int annotationId);
+    void updateAnnotationRequested(const Annotation &annotation);
 
 protected:
     void enterEvent(QEnterEvent *event) override;
     void leaveEvent(QEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseDoubleClickEvent(QMouseEvent *event) override;
 
 private:
+    enum class Mode {
+        Minimized,
+        Extended,
+        Edited
+    };
+
     void refreshContent();
+    void setMode(Mode mode);
+    void applyEdits();
+    void adjustNoteEditHeight();
+    void openColorPicker();
+    void updateColorButton();
     void openMenu();
     double currentFps() const;
 
@@ -41,13 +58,31 @@ private:
     bool m_isDarkMode = true;
     QColor m_palbtnColor;
     QString m_palbtnColorStr;
+ 
+    Mode m_mode = Mode::Minimized;
+    QTimer* m_clickTimer = nullptr;
+    bool m_ignoreNextRelease = false;
+
+    QHBoxLayout* m_topRowLayout = nullptr;
+    QVBoxLayout* m_timeLayout = nullptr;
 
     QFrame* m_colorBar = nullptr;
     QLabel* m_startLabel = nullptr;
     QLabel* m_endLabel = nullptr;
     QLabel* m_nameLabel = nullptr;
     QLabel* m_noteLabel = nullptr;
-    ToolbarButton* m_menuBtn = nullptr;
+    ToolbarButton* m_updateBtn = nullptr;
+
+    // edit mode
+    QColor m_editColor;
+    QLineEdit* m_startEdit = nullptr;
+    QLineEdit* m_endEdit = nullptr;
+    QLineEdit* m_nameEdit = nullptr;
+    QTextEdit* m_noteEdit = nullptr;
+    QPushButton* m_colorBtn = nullptr;
+    ToolbarButton* m_confirmBtn = nullptr;
+    ToolbarButton* m_cancelBtn = nullptr;
+    ToolbarButton* m_deleteBtn = nullptr;
 };
 
 #endif // ANNOTATIONWIDGET_H

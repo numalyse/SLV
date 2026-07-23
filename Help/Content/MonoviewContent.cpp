44552fc01152c6bbaa288a628569a7d370a219e3
Christinamrn <58227221+Christinamrn@users.noreply.github.com>
2026-07-23 15:59:05 +0200

    Delete classic toolbar from MonoviewContent

--- Help/Content/MonoviewContent.cpp
+++ Help/Content/MonoviewContent.cpp
@@ -21,11 +21,6 @@ MonoviewContent::MonoviewContent(QWidget* parent)
         player()
     );
 
-    addSubcategory(
-        "help_menu_classic_toolbar_buttons_descriptions",
-        classicBar()
-    );
-
     
 }
 
@@ -47,44 +42,4 @@ QWidget* MonoviewContent::player()
     layout->addWidget(new QLabel("Test"));
 
     return widget;
-}
-
-QWidget* MonoviewContent::classicBar()
-{
-    QWidget* widget = new QWidget;
-    auto* layout = new QVBoxLayout(widget);
-
-    auto* buttonsDescriptionTable = new ContentBase;
-
-    layout->addWidget(buttonsDescriptionTable->createButtonDescriptionTable(
-        "help_menu_classic_toolbar_left_buttons", {
-        {":/icons/sound_on", "help_menu_volume_label", "help_menu_volume_description"},
-        {":/icons/lang", "help_menu_language_label", "help_menu_language_description"},
-        {":/icons/media_info", "help_menu_media_info_label", "help_menu_media_info_description"}
-    }));
-
-    layout->addWidget(buttonsDescriptionTable->createButtonDescriptionTable(
-        "help_menu_classic_toolbar_central_buttons", {
-        {":/icons/slow", "help_menu_speed_label", "help_menu_speed_description"},
-        {":/icons/play", "help_menu_play_label", "help_menu_play_description"},
-        {":/icons/pause", "help_menu_pause_label", "help_menu_pause_description"},
-        {":/icons/stop", "help_menu_stop_label", "help_menu_stop_description"},
-        {":/icons/eject", "help_menu_eject_label", "help_menu_eject_description"},
-        {":/icons/loop_off", "help_menu_loop_label", "help_menu_loop_description"},
-    }));
-
-    layout->addWidget(buttonsDescriptionTable->createButtonDescriptionTable(
-        "help_menu_classic_toolbar_right_buttons", {
-        {":/icons/zoom", "help_menu_zoom_label", "help_menu_zoom_description"},
-        {":/icons/capture", "help_menu_screenshot_label", "help_menu_screenshot_description"},
-        {":/icons/extract_sequence", "help_menu_extraction_label", "help_menu_extraction_description"},
-        {":/icons/duplicate_media", "help_menu_duplicate_label", "help_menu_duplicate_description"},
-        {":/icons/fullscreen", "help_menu_fullscreen_label", "help_menu_fullscreen_description"},
-        {":/icons/right_arrow", "help_menu_extended_label", "help_menu_extended_description"},
-    }));
-
-    //layout->addWidget(buttonsDescriptionTable);
-
-    return widget;
-
 }

52bebd2139fa00b11448e440ba003a3dacc2800b
Christinamrn <58227221+Christinamrn@users.noreply.github.com>
2026-08-28 16:14:53 +0200

    Update MonoviewContent.cpp

--- Help/Content/MonoviewContent.cpp
+++ Help/Content/MonoviewContent.cpp
@@ -11,7 +11,7 @@ MonoviewContent::MonoviewContent(QWidget* parent)
     : CategoryBase("help_menu_classic_category", parent)
 {
     QString presentationTitle = "help_menu_presentation_label";
-    QString openMedia = "help_menu_open_files_label";
+    QString openMedia = "help_menu_open_file_label";
 
     addSubcategory(
         presentationTitle,
@@ -50,10 +50,10 @@ QWidget* MonoviewContent::presentation(const QString& subcategoryName)
 
     widget->addTextFromLangQMAP({
         {"fr", 
-            "Le mode mono-vue est le mode d'affichage par défaut du lecteur multimédias.\n"
+            "Le mode mono-vue est le mode d'affichage par défaut du lecteur multimédia.\n"
             "Il permet de visualiser un seul média à la fois et d'utiliser différents outils pour travailler et interagir avec celui-ci. \n"
             "Sa barre d'outils peut être étendue afin d'accéder à des fonctionnalités supplémentaires. \n"
-            "Parmi ces outils, il est notamment possible d'accéder à une timeline détaillée, permettant de naviguer plus précisément dans le média et de visualiser son déroulement."
+            "Parmi ces outils, il est notamment possible d'accéder à une timeline détaillée, permettant de naviguer plus précisément dans le média et de visualiser son déroulement.\n"
             "Il est également possible d'accéder à un affichage en playlist, permettant d'enchaîner la lecture de plusieurs médias à la suite."
         },
         {"en", 
@@ -84,8 +84,7 @@ QWidget* MonoviewContent::openmedia(const QString& subcategoryName)
 
     widget->addTextFromLangQMAP({
         {"fr", 
-            "Il existe différentes façon d'ouvrir un média. \n"
-            "Faire cela."
+            "Pour ouvrir un média, plusieurs méthodes sont disponibles.\n"
         },
         {"en", 
             ""
@@ -106,14 +105,74 @@ QWidget* MonoviewContent::openmedia(const QString& subcategoryName)
 
     //widget->addTextFromLangQMAP(title);
 
+    widget->addQMAPTexts({
+        {"fr", {"Depuis le menu Fichier",
+            "Cliquer sur la barre de menu « Fichier », sélectionner « Ouvrir un média… » pour parcourir les fichiers disponibles sur l'ordinateur. Sélectionner ensuite le média souhaité pour l'ouvrir dans l'application.\n"}
+        },
+        {"en", {"",
+            ""}
+        },
+        {"es", {"",
+            ""}
+        },
+        {"de", {"",
+            ""}
+        },
+        {"it", {"",
+            ""}
+        },
+        {"pt", {"",
+            ""}
+        }
+    });
+
     widget->addImage("open_medias");
 
-    widget->addTextFromLangJSON("help_menu_open_files_content_2");
+    widget->addQMAPTexts({
+        {"fr", {"Par glisser-déposer",
+            "Un média peut également être ouvert directement par glisser-déposer. Faire glisser le fichier depuis son emplacement sur l'ordinateur, puis le déposer dans la fenêtre de l'application. Le média est alors automatiquement chargé.\n"}
+        },
+        {"en", {"",
+            ""}
+        },
+        {"es", {"",
+            ""}
+        },
+        {"de", {"",
+            ""}
+        },
+        {"it", {"",
+            ""}
+        },
+        {"pt", {"",
+            ""}
+        }
+    });
+
     widget->addImage("open_medias_drag_drop");
 
-    widget->addSectionName("help_menu_formats_label");
-    
-    widget->getFormatsAvailables();
+    widget->addQMAPTexts({
+        {"fr", {"Avec le bouton Lecture",
+            "Lorsqu'aucun média n'est déjà ouvert, le bouton Lecture permet également d'ouvrir un fichier. Cliquer sur ce bouton pour afficher le sélecteur de fichiers, puis choisir le média à ouvrir.\n"}
+        },
+        {"en", {"",
+            ""}
+        },
+        {"es", {"",
+            ""}
+        },
+        {"de", {"",
+            ""}
+        },
+        {"it", {"",
+            ""}
+        },
+        {"pt", {"",
+            ""}
+        }
+    });
+
+        widget->addImage("open_medias_play_button");
 
     return widget;
 }
